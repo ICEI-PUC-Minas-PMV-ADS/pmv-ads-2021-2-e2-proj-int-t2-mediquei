f0dc171c4cc8e6c80c15ba5bf8e19d36f9c7fd10
Jaqueline Poletto <poletto.jaque@gmail.com>
2021-09-29 22:06:04 -0300

    Ajuste no esquema relacional
    Ajuste no esquema relacional

--- docs/diagramas/sql_create_mediquei.sql
+++ docs/diagramas/sql_create_mediquei.sql
@@ -8,37 +8,6 @@ UserName VARCHAR(10) PRIMARY KEY,
 Password VARCHAR(10)
 )
 
-CREATE TABLE Cuidador (
-codCuidador INTEGER PRIMARY KEY,
-nomeCuidador VARCHAR(30),
-UserName VARCHAR(10),
-FOREIGN KEY(UserName) REFERENCES Usuario (UserName)
-)
-
-CREATE TABLE Tratamento (
-codPaciente INTEGER,
-dataInicial DATETIME,
-codDesafio INTEGER,
-codRemedio INTEGER,
-codEfeito1 INTEGER,
-codEfeito2 INTEGER,
-codEfeito3 INTEGER,
-Observacao Text,
-PRIMARY KEY(codPaciente,dataInicial,codDesafio,codRemedio)
-)
-
-CREATE TABLE Paciente (
-codPaciente INTEGER PRIMARY KEY,
-nomePaciente VARCHAR(30),
-UserName VARCHAR(10),
-FOREIGN KEY(UserName) REFERENCES Usuario (UserName)
-)
-
-CREATE TABLE DesafioSaude (
-codDesafio INTEGER PRIMARY KEY,
-nomeDesafio VARCHAR(40)
-)
-
 CREATE TABLE Familiar (
 codFamiliar INTEGER PRIMARY KEY,
 nomeFamiliar VARCHAR(30),
@@ -46,18 +15,11 @@ UserName VARCHAR(10),
 FOREIGN KEY(UserName) REFERENCES Usuario (UserName)
 )
 
-CREATE TABLE ContratoCuidador (
-codPaciente INTEGER,
-codCuidador INTEGER,
-dataInicial DATETIME,
-dataFinal DATETIME,
-horaEntrada1 Time,
-horaSaida1 time,
-horaEntrada2 Time,
-horaSaida2 Time,
-PRIMARY KEY(codPaciente,codCuidador,dataInicial),
-FOREIGN KEY(codPaciente) REFERENCES Paciente (codPaciente),
-FOREIGN KEY(codCuidador) REFERENCES Cuidador (codCuidador)
+CREATE TABLE Cuidador (
+codCuidador INTEGER PRIMARY KEY,
+nomeCuidador VARCHAR(30),
+UserName VARCHAR(10),
+FOREIGN KEY(UserName) REFERENCES Usuario (UserName)
 )
 
 CREATE TABLE EfeitoAdverso (
@@ -75,9 +37,48 @@ concentracao VARCHAR(20),
 formaFarmaceutica VARCHAR(20)
 )
 
-ALTER TABLE Tratamento ADD FOREIGN KEY(codPaciente) REFERENCES Paciente (codPaciente)
-ALTER TABLE Tratamento ADD FOREIGN KEY(codDesafio) REFERENCES DesafioSaude (codDesafio)
-ALTER TABLE Tratamento ADD FOREIGN KEY(codRemedio) REFERENCES Remedio (codRemedio)
-ALTER TABLE Tratamento ADD FOREIGN KEY(codEfeito1) REFERENCES EfeitoAdverso (codEfeito)
-ALTER TABLE Tratamento ADD FOREIGN KEY(codEfeito2) REFERENCES EfeitoAdverso (codEfeito)
-ALTER TABLE Tratamento ADD FOREIGN KEY(codEfeito3) REFERENCES EfeitoAdverso (codEfeito)
+CREATE TABLE DesafioSaude (
+codDesafio INTEGER PRIMARY KEY,
+nomeDesafio VARCHAR(40)
+)
+
+CREATE TABLE Paciente (
+codPaciente INTEGER PRIMARY KEY,
+nomePaciente VARCHAR(30),
+UserName VARCHAR(10),
+codFamiliar1 INTEGER,
+FOREIGN KEY(UserName) REFERENCES Usuario (UserName),
+FOREIGN KEY(codFamiliar1) REFERENCES Familiar (codFamiliar)
+)
+
+CREATE TABLE Tratamento (
+codPaciente INTEGER,
+dataInicial DATETIME,
+codDesafio INTEGER,
+codRemedio INTEGER,
+codEfeito1 INTEGER,
+codEfeito2 INTEGER,
+codEfeito3 INTEGER,
+Observacao Text,
+PRIMARY KEY(codPaciente,dataInicial,codDesafio,codRemedio),
+FOREIGN KEY(codPaciente) REFERENCES Paciente (codPaciente),
+FOREIGN KEY(codDesafio) REFERENCES DesafioSaude (codDesafio),
+FOREIGN KEY(codRemedio) REFERENCES Remedio (codRemedio),
+FOREIGN KEY(codEfeito1) REFERENCES EfeitoAdverso (codEfeito),
+FOREIGN KEY(codEfeito2) REFERENCES EfeitoAdverso (codEfeito),
+FOREIGN KEY(codEfeito3) REFERENCES EfeitoAdverso (codEfeito)
+)
+
+CREATE TABLE ContratoCuidador (
+codPaciente INTEGER,
+codCuidador INTEGER,
+dataInicial DATETIME,
+dataFinal DATETIME,
+horaEntrada1 Time,
+horaSaida1 time,
+horaEntrada2 Time,
+horaSaida2 Time,
+PRIMARY KEY(codPaciente,codCuidador,dataInicial),
+FOREIGN KEY(codPaciente) REFERENCES Paciente (codPaciente),
+FOREIGN KEY(codCuidador) REFERENCES Cuidador (codCuidador)
+)
