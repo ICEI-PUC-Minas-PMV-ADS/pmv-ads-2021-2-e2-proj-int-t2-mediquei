056d4ca83f19b892b7b36ff2e58c87e65097baa8
adrianansc <adriana.nsc@gmail.com>
2021-11-06 20:57:24 -0300

    tentaivas de front no boostrap

--- Projeto/app-web-backend-Mediquei/wwwroot/js/site.js
+++ Projeto/app-web-backend-Mediquei/wwwroot/js/site.js
@@ -2,3 +2,32 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+
+$(document).ready(function () {
+    $("#signup-form").submit(function () {
+        var nm1 = $("#name1").val();
+        var ps1 = $("#pass1").val();
+        localStorage.setItem("n1", nm1);
+        localStorage.setItem("p1", ps1);
+
+    });
+
+    $("#login-form").submit(function (event) {
+        event.preventDefault()
+        var enteredName = $("#name2").val();
+        var enteredPass = $("#pass2").val();
+
+        var storedName = localStorage.getItem("n1");
+        var storedPass = localStorage.getItem("p1");
+
+        if (enteredName == storedName && enteredPass == storedPass) {
+            alert("Logado com sucesso");
+            window.location.href = "/homelogin.html";
+        }
+        else {
+            alert("Dados não encontrados");
+        }
+
+    });
+
+});
