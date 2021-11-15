1c1dd232739312d1a5744f0d3e309c1abdcfa0a7
JaquelinePoletto <79724847+JaquelinePoletto@users.noreply.github.com>
2021-11-15 20:48:29 -0300

    Paciente/Cuidador
    Cadastro de Paciente, Familiar e Contrato do Cuidador

---
+++ Projeto/app-web-backend-Mediquei/Models/Paciente.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace app_web_backend_Mediquei.Models
+{
+    [Table("Pacientes")]
+    public class Paciente
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required(ErrorMessage = "*Campo obrigatório!")]
+        public string Nome { get; set; }
+
+        /* Criando a chave estrangeira para usuário */
+        [Display(Name = "Usuário")]
+        [Required(ErrorMessage = "*Campo Obrigatório")]
+        public int UserId { get; set; }
+        [ForeignKey("UserId")]
+        public Usuario Usuario { get; set; }
+
+        /* Criando a chave estrangeira para familiar */
+        [Display(Name = "Familiar")]
+        public int FamiliarId { get; set; }
+        [ForeignKey("FamiliarId")]
+        public Familiar Familiar { get; set; }
+        
+        [Display(Name = "Grau de Parentesco")]
+        public GrauParentesco grauParentesco { get; set; }
+    }
+
+    public enum GrauParentesco
+    {
+        Pai,
+        Mae,
+        Filho,
+        Filha,
+        Sobrinho,
+        Sobrinha,
+        Enteado,
+        Enteada,
+        Sogro,
+        Sogra,
+        Irmão,
+        Irmã
+    }
+}
